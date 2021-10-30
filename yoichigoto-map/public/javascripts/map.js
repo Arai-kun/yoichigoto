098bc1cba4e999cc63bc14477b18fbb3bf10a84d
Arai-kun <koki.alright@gmail.com>
2021-10-31 02:25:12 +0900

    first

---
+++ yoichigoto-map/public/javascripts/map.js
@@ -0,0 +1,72 @@
+/**
+ * 2021/10/31
+ * Call API foward to server form specified URL query parameters 
+ * and reflect them to map after recieving GeoJSON array.
+ * You only need mapbox api key to work this program.
+ */
+
+const query = window.location.search;
+if(query !== '')
+{
+    get_segments(query)
+    .then(geojson_arr => {
+        mapboxgl.accessToken = 'pk.eyJ1IjoiYXJhaS1rdW4iLCJhIjoiY2t2ZG1ycDVhNDhoNTJ1cTF1Zms4ZGR5bSJ9.w0CSkX2GQr21qW_DfEJ4ZA';
+        let map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: geojson_arr[0]['coordinates'][0],
+            zoom: 11
+        });
+        map.on('load', () => {
+        for(let i = 0; i < geojson_arr.length; i++)
+            {
+                map.addSource(`route${i}`, {
+                    'type': 'geojson',
+                    'data': geojson_arr[i]
+                });
+                map.addLayer({
+                    'id': `route${i}`,
+                    'type': 'line',
+                    'source': `route${i}`,
+                    'layout': {
+                        'line-join': 'round',
+                        'line-cap': 'round'
+                    },
+                    'paint': {
+                    'line-color': '#ff0000',
+                    'line-width': 7
+                    }
+                });
+            }
+        });
+    })
+    .catch(error =>{
+        console.log(error);
+    });
+}
+
+async function get_segments(bounds_params)
+{
+    const params = new URLSearchParams(bounds_params);
+    let data = {};
+    if(params.has('sw_lat') && params.has('sw_long') && params.has('ne_lat') && params.has('ne_long'))
+    {
+        data = {
+            'sw_lat': params.get('sw_lat'),
+            'sw_long': params.get('sw_long'),
+            'ne_lat': params.get('ne_lat'),
+            'ne_long': params.get('ne_long'),
+            'activity_type': 'running'
+        }
+        /* Call Strava API anf Decode */
+        const res = await fetch('/api', {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify(data)
+        });
+        /* Recieve GeoJson Array */
+        return res.json();
+    }
+}
---
+++ yoichigoto-map/routes/api.js
@@ -0,0 +1,89 @@
+/**
+ * 2021/10/31
+ * As test at first, hardcording kinds of auth information.
+ */
+
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+var polyline = require('@mapbox/polyline');
+let geojson_arr = [];
+let access_token = '';
+const refresh_token = '6b91ab4184d266b1ca5edd84aded51f1d020de3b';
+
+/* GET api listing. */
+router.post('/', function(req, res, next) {
+  refreshToken()
+  .then(() => {
+    getSegments(req.body)
+    .then(() => {
+      res.json(geojson_arr);
+    })
+    .catch(error => {
+      console.log(error);
+      res.sendStatus(500);
+    });
+  })
+  .catch(error => {
+    console.log(error);
+    res.sendStatus(500);
+  });
+});
+
+module.exports = router;
+
+async function refreshToken()
+{
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    url: 'https://www.strava.com/api/v3/oauth/token',
+    body: JSON.stringify({
+      'client_id': 71712,
+      'client_secret': 'ab05751c172e2ba2b20e670d40b415f035425d12',
+      'grant_type': 'refresh_token',
+      'refresh_token': refresh_token
+    })
+  }
+  await new Promise((resolve, reject) => {
+    request(options, (error, response, body) => {
+      access_token = JSON.parse(body)['access_token'];
+      resolve();
+      if(error)
+      {
+        reject(error);
+      }
+    });
+  });
+}
+
+async function getSegments(query_json)
+{
+  const url = 'https://www.strava.com/api/v3/segments/explore' + 
+    `?bounds=${query_json['sw_lat']},${query_json['sw_long']},${query_json['ne_lat']},${query_json['ne_long']}&activity_type=${query_json['activity_type']}`;
+  const options = {
+    url: url,
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${access_token}`
+    },
+    method: 'GET'
+  }
+  await new Promise((resolve, reject) => {
+    request(options, (error, response, body) => {
+      let data = [];
+      data = JSON.parse(body)['segments'];
+      for (let i= 0; i < data.length; i++)
+      {
+        geojson_arr.push(polyline.toGeoJSON(data[i]['points']));
+      }
+      resolve();
+      if(error)
+      {
+        reject(error);
+      }
+    });
+  });
+}
