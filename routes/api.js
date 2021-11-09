6101431e372e8f8348c6d048f93189d30012ca25
Arai-kun <koki.alright@gmail.com>
2021-11-10 00:09:18 +0900

    extended strava api

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,5 @@
 /**
- * 2021/10/31
+ * 2021/11/09
  * As test at first, hardcording kinds of auth information.
  */
 
@@ -8,22 +8,52 @@ var router = express.Router();
 var request = require('request');
 var polyline = require('@mapbox/polyline');
 let geojson_arr = [];
+let latlong_arr = [];   /* [[{x: , y: }, {x: , y: }],... ] */
+let divCount = 0;
+let acType = '';
 let access_token = '';
 const refresh_token = '6b91ab4184d266b1ca5edd84aded51f1d020de3b';
-const earth_radius = 6378.137;   /* km */
+const EARTH_RADIUS = 6378.137;   /* km */
+const MIN_UNIT = 2;  /* km */
 
 /* GET api listing. */
 router.post('/', function(req, res, next) {
+  let latlong = [
+    {x: parseFloat(req.body['sw_long']), y: parseFloat(req.body['sw_lat'])},
+    {x: parseFloat(req.body['ne_long']), y: parseFloat(req.body['ne_lat'])}
+  ];
+  latlong_arr.push(latlong);
   refreshToken()
   .then(() => {
-    getSegments(req.body)
-    .then(() => {
-      res.json(geojson_arr);
-    })
-    .catch(error => {
-      console.log(error);
-      res.sendStatus(500);
-    });
+    while((calDistance(latlong_arr[latlong_arr.length - 1]) / Math.sqrt(2)) > MIN_UNIT)
+    {
+      for(let i = 0; i < 4 ** divCount; i++)
+      {
+        divArea(latlong_arr[i + (((4 ** divCount) - 1) / 3)]);
+      }
+      divCount++;
+    }
+    console.log(latlong_arr.length);
+    //console.log(latlong_arr);
+    acType = req.body['activity_type'];
+    getAllSegments()
+      .then(() => {
+        res.json(geojson_arr);
+      })
+      .catch(error => {
+        console.log(error);
+        res.sendStatus(500);
+      });
+    /*
+      getSegments(latlong_arr[0], req.body['activity_type'])
+      .then(() => {
+        res.json(geojson_arr);
+      })
+      .catch(error => {
+        console.log(error);
+        res.sendStatus(500);
+      });
+    */
   })
   .catch(error => {
     console.log(error);
@@ -60,10 +90,11 @@ async function refreshToken()
   });
 }
 
-async function getSegments(query_json)
+async function getSegments(longlat, type)
 {
   const url = 'https://www.strava.com/api/v3/segments/explore' + 
-    `?bounds=${query_json['sw_lat']},${query_json['sw_long']},${query_json['ne_lat']},${query_json['ne_long']}&activity_type=${query_json['activity_type']}`;
+    `?bounds=${longlat[0].y},${longlat[0].x},${longlat[1].y},${longlat[1].x}&activity_type=${type}`;
+  console.log(url);
   const options = {
     url: url,
     headers: {
@@ -89,12 +120,49 @@ async function getSegments(query_json)
   });
 }
 
+async function getAllSegments()
+{
+  for await (let latlong of latlong_arr)
+  {
+    console.log(latlong);
+    getSegments(latlong, acType);
+  }
+}
+
 /**
  * Calculate a distance between two points
- * @param {Array} latlongs [[x1, y1], [x2, y2]]
- * @returns distance
+ * @param {Array} latlongs [{lat:x1, long:y1}, {lat:x2, long:y2}]
+ * @returns distance(km)
  */
-function calDistance(latlongs)
+function calDistance(latlong)
 {
-  return earth_radius * Math.acos(Math.sin(latlongs[0][1]) * Math.sin(latlongs[1][1]) + Math.cos(latlongs[0][1]) * Math.cos(latlongs[1][1]) * Math.cos((latlongs[1][0] - latlongs[0][0])));
+  return EARTH_RADIUS * Math.acos(Math.sin(latlong[0].y * (Math.PI / 180))
+   * Math.sin(latlong[1].y * (Math.PI / 180)) + Math.cos(latlong[0].y * (Math.PI / 180))
+   * Math.cos(latlong[1].y * (Math.PI / 180)) * Math.cos((latlong[1].x - latlong[0].x) * (Math.PI / 180)));
+}
+
+function divArea(latlong)
+{
+  let buf_x = (latlong[1].x + latlong[0].x) / 2;
+  let buf_y = (latlong[1].y + latlong[0].y) / 2;
+  
+  latlong_arr.push([
+    {x: latlong[0].x, y: buf_y},
+    {x: buf_x, y: latlong[1].y}
+  ]);
+
+  latlong_arr.push([
+    {x: buf_x, y: buf_y},
+    {x: latlong[1].x, y: latlong[1].y}
+  ]);
+
+  latlong_arr.push([
+    {x: buf_x, y: latlong[0].y},
+    {x: latlong[1].x, y: buf_y}
+  ]);
+
+  latlong_arr.push([
+    {x: latlong[0].x, y: latlong[0].y},
+    {x: buf_x, y: buf_y}
+  ]);
 }
